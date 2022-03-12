87a2c2628311be0aa002466023e0f014871bc460
Yucheng Zhu <YZ_YuchengZhu@outlook.com>
2022-03-13 04:17:40 +0800

    Substantiating the exercise.

--- utils/data_cleansing.py
+++ utils/data_cleansing.py
@@ -23,3 +23,41 @@ def SeparateNumericAndNonNumericFeatures(df_train, drops=None):
             len(df_train.columns) - len(drops)
     return features_numeric, features_non_numeric
     
+# -- 'Embarked': C, Q, S -> 0.553571, 0.389610, 0.336957
+def EncodeNumericalCategoricalVariables(
+    df_train, 
+    NumericalCategoricalVariableName,
+    NameToCalculateResultRate=None,
+    SurvivalRatesForAllCategories=None
+):
+    df_train_numericized = df_train.copy(deep=True)
+    n = NumericalCategoricalVariableName
+    
+    Grouped = df_train.groupby(
+        n
+    ).sum()
+    if SurvivalRatesForAllCategories is None:
+        actual_name, total_name = NameToCalculateResultRate
+        SurvivalRatesForAllCategories = \
+            Grouped[actual_name] / Grouped[total_name]
+    
+    for i in Grouped.index:
+        df_train_numericized.loc[
+            df_train[n].values == i,
+            n
+        ] = SurvivalRatesForAllCategories[i]
+    df_train_numericized[n] = \
+        df_train_numericized[n].astype('float64')
+    return df_train_numericized, SurvivalRatesForAllCategories
+# df_train_numericized, SurvivalRatesForAllCategories = EncodeNumericalCategoricalVariables(
+    # df_train_preparsed, 
+    # 'Embarked',
+    # NameToCalculateResultRate=('Survived','Count')
+# )
+# df_test_numericized, _ = EncodeNumericalCategoricalVariables(
+    # df_test_preparsed, 
+    # 'Embarked',
+    # SurvivalRatesForAllCategories=SurvivalRatesForAllCategories
+# )
+# df_train_numericized.head(5)
+
