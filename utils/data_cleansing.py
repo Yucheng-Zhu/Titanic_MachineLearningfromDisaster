2e678e3d0e66858a467ce531f8aeb29f00f14c1c
Yucheng Zhu <YZ_YuchengZhu@outlook.com>
2022-03-13 03:11:49 +0800

    Substantiating the exercise.

--- utils/data_cleansing.py
+++ utils/data_cleansing.py
@@ -7,3 +7,19 @@ def ReplaceNaNs(df):
     df_clean = df_clean.fillna('NaN')
     return df_clean
 
+def SeparateNumericAndNonNumericFeatures(df_train, drops=None):
+    import numpy as np
+    numeric_types = (np.number, bool)
+    if drops == None:
+        drops = []
+    df_train_forSeparation = df_train.drop(columns=drops)
+    features_numeric = \
+        df_train_forSeparation.select_dtypes(numeric_types).columns
+    
+    features_non_numeric = \
+        df_train_forSeparation.select_dtypes(exclude=numeric_types).columns
+    
+    assert len(features_numeric) + len(features_non_numeric) == \
+            len(df_train.columns) - len(drops)
+    return features_numeric, features_non_numeric
+    
